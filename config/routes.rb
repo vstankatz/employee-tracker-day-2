e3bc20f372acc351edc668b3ff31f8a02574ca0b
Student <example@example.com>
2020-01-16 11:59:52 -0800

    Set new homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'divisions#index'
+  root 'homes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :divisions do
     resources :employees
