214229a119232086e361c05704b821594b4db265
Student <example@example.com>
2020-01-16 09:58:10 -0800

    Add scopes

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,23 @@ class Employee < ApplicationRecord
   has_many :employee_projects
   has_many :projects, :through => :employee_projects, dependent: :destroy
   validates :name, :division_id, presence: true
+
+  scope :start_letter, -> (letter_parameter) { where("name ilike ?", "#{letter_parameter}%")}
+  scope :most_projects, -> do (
+    select("employees.id, employees.name, count(projects.id) as projects_count")
+    .joins(:projects)
+    .group("employees.id")
+    .order("projects_count DESC")
+    .limit(10))
+  end
+
+  scope :tenure, -> (division_id_paramater) do (
+    where(division_id: division_id_paramater)
+    .order("created_at ASC"))
+  end
+
+  scope :newest, -> (division_id_paramater) do (
+    where(division_id: division_id_paramater)
+    .order("created_at DESC"))
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,12 @@
 class Project < ApplicationRecord
   has_many :employee_projects
   has_many :employees, :through => :employee_projects, dependent: :destroy
+
+  scope :most_employees, -> {(
+    select("projects.id, projects.name, count(employees.id) as employees_count")
+    .joins(:employees)
+    .group("projects.id")
+    .order("employees_count DESC")
+    .limit(10)
+  )}
 end
