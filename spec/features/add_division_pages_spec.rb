d87916faa699cf7b9127518c25a63241693d33bd
Student <example@example.com>
2020-01-22 16:30:39 -0800

    Add simplecov gem and edit specs to work with devise

--- spec/features/add_division_pages_spec.rb
+++ spec/features/add_division_pages_spec.rb
@@ -4,6 +4,11 @@ describe "the add a division process" do
   it "adds a new division" do
     visit divisions_path
     click_link 'Create new division'
+    click_link 'Sign up'
+    fill_in 'user[email]', :with => 'test@epicodus.com'
+    fill_in 'user[password]', :with => 'password'
+    fill_in 'user[password_confirmation]', :with => 'password'
+    click_on 'Sign up'
     fill_in 'division[name]', :with => 'HR'
     click_on 'Create Division'
     expect(page).to have_content 'HR division successfully added!'
@@ -12,6 +17,11 @@ describe "the add a division process" do
 
   it "gives an error when no name is entered" do
     visit new_division_path
+    click_link 'Sign up'
+    fill_in 'user[email]', :with => 'test@epicodus.com'
+    fill_in 'user[password]', :with => 'password'
+    fill_in 'user[password_confirmation]', :with => 'password'
+    click_on 'Sign up'
     click_on 'Create Division'
     expect(page).to have_content "Name can't be blank"
   end
